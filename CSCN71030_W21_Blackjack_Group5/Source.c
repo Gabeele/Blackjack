83dbd01a992008b053b7b6ae6cf483479f3a0b7b
Gavin Abeele <Gabeele2160@conestogac.on.ca>
2021-03-28 19:55:42 -0400

    Fixed double main() issue.

--- CSCN71030_W21_Blackjack_Group5/Source.c
+++ CSCN71030_W21_Blackjack_Group5/Source.c
@@ -2,29 +2,27 @@
 //
 //Group 5
 
-int main() {
+#include "GUI.h"
+//#include "player.h"
+//#include "playerNode.h"
+//#include "playerList.h"
+//#include "dataValidation.h"
+//#include "gameMechanics.h"
+//#include "fileIO.h"
 
-	//#include "GUI.h"
-	//#include "player.h"
-	//#include "playerNode.h"
-	//#include "playerList.h"
-	//#include "dataValidation.h"
-	//#include "gameMechanics.h"
-	//#include "fileIO.h"
+int main(int argc, char** argv) {
 
-	int main(int argc, char** argv) {
+	pPlayerList playerList = initalization();	//Creates, inisitalizes, and reads from the file and returns a playerList
 
-		pPlayerList playerList = initalization();	//Creates, inisitalizes, and reads from the file and returns a playerList
+	pPlayer player = selectPlayerMenu(cmdLnPlayer, playerList);	//Selects the player from the list either through the menu or through the command line argument
 
-		pPlayer player = selectPlayerMenu(cmdLnPlayer, playerList);	//Selects the player from the list either through the menu or through the command line argument
+	while (1) {
 
-		while (1) {
+		mainMenu(playerList, player);
 
-			mainMenu(playerList, player);
-
-		}
-
-		//Comment
-
-		return 0;
 	}
+
+	//Comment
+
+	return 0;
+}

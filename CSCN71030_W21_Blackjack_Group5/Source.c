ff5fd8326721bfd7391ba81f1affab11ba7c0fd6
Gavin Abeele <59030389+Gabeele@users.noreply.github.com>
2021-03-28 19:46:55 -0400

    Added Deleted Source
    Upon testing a pull request, I unintentionally deleted the source code. Pasted it back into the file.

---
+++ CSCN71030_W21_Blackjack_Group5/Source.c
@@ -0,0 +1,30 @@
+//CSCN71030 - Project II 
+//
+//Group 5
+
+int main() {
+
+	//#include "GUI.h"
+	//#include "player.h"
+	//#include "playerNode.h"
+	//#include "playerList.h"
+	//#include "dataValidation.h"
+	//#include "gameMechanics.h"
+	//#include "fileIO.h"
+
+	int main(int argc, char** argv) {
+
+		pPlayerList playerList = initalization();	//Creates, inisitalizes, and reads from the file and returns a playerList
+
+		pPlayer player = selectPlayerMenu(cmdLnPlayer, playerList);	//Selects the player from the list either through the menu or through the command line argument
+
+		while (1) {
+
+			mainMenu(playerList, player);
+
+		}
+
+		//Comment
+
+		return 0;
+	}
